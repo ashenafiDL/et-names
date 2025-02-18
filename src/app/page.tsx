cbaca8ee936d567cc5f9161155831a1ddd392017
ashenafiDL <ashenafi.debella101@gmail.com>
2025-02-18 03:30:29 +0300

    feat: add basic layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@ import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div className="flex items-center justify-center h-screen w-screen">
-      <Button>et-names</Button>
+    <div className="">
+      Home
     </div>
   );
 }
